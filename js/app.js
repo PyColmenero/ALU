64c724eb4c04c7361f21c471d83e41eb7dc5d2fe
PyColmenero <pycolmenero@gmail.com>
2023-01-10 00:34:44 +0100

    JS que arrancar la app

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const dragDroper = new DragDroper();
+const board = new Board();
+
+setTimeout(() => {
+
+    // $("#BULB").click();
+    // $("#BULB").click();
+    $("#AND").click();
+    
+    setTimeout(() => {
+
+        // $("#NOT").click();
+        // $("#BULB").click();
+
+        // setTimeout(() => {
+
+        //     $("#AND").click();
+
+        // }, 1);
+
+    }, 1);
+
+}, 1);
+
+// function radians_to_degrees(radians) {
+//     var pi = Math.PI;
+//     return radians * (180 / pi);
+// }
